dccade2569e716fa86e2ba06c6a8cadc2f6d9cda
kvoncode <kvoncode@gmail.com>
2023-10-20 13:29:20 +0900

    add: saving, retrieving from local storage

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ const endButton = document.getElementById("endButton");
 const display = document.getElementById("display");
 
 let bufferAtom = {};
+// Storing Array of Atoms here
+let localStorageAtoms = [];
+
+// Get json string of atoms from Local Storage
+const jsonLocalStorage = localStorage.getItem("atoms");
+
+// Parse array of atoms
+const parsedAtoms = JSON.parse(jsonLocalStorage);
+
+// check if already have array of atoms stored in Local Storage
+if (Array.isArray(parsedAtoms)) {
+  localStorageAtoms = parsedAtoms;
+}
+
+const stringifiedAtoms = JSON.stringify(localStorageAtoms);
+
+display.innerHTML = stringifiedAtoms;
 
 startButton.onclick = () => {
   //   console.log("the value of the title", atomTitle.value);
@@ -13,7 +30,7 @@ startButton.onclick = () => {
     start: new Date(),
   };
 
-  console.log("newAtom", newAtom);
+  // console.log("newAtom", newAtom);
 
   bufferAtom = newAtom;
 };
@@ -23,7 +40,16 @@ endButton.onclick = () => {
 
   console.log("bufferAtom", bufferAtom);
 
+  // Push to global array
+  const atomsArray = localStorageAtoms.push(bufferAtom);
   // Display Atoms
+  const stringifiedAtoms = JSON.stringify(localStorageAtoms);
 
-  display.innerHTML = JSON.stringify(bufferAtom);
+  display.innerHTML = stringifiedAtoms;
+
+  // Save to Local Storage
+  localStorage.setItem("atoms", JSON.stringify(localStorageAtoms));
+
+  const savedAtoms = localStorage.getItem("atoms");
+  console.log("savedAtoms", savedAtoms);
 };
